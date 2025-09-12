4c8d44cb869012374e151d9bc21b68df40f3a4df
areeba-amirr <areebaamir826@gmail.com>
2025-09-12 23:26:24 +0500

    Variables and DatasTypes

---
+++ variable.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //declaration
+  int var1;
+  //initialization
+  var1 = 10;
+  //declaration + initialization
+  int var2 = 20;
+
+  //Data types
+  int no = 10;
+  float point = 2.3;
+  char ch = 'a';
+  string str = "Areeba Aamir";
+  double longPoint = 223.555555555;
+  bool is =  false; //return 1 or 0
+  cout<<var1<<"\n";
+  cout<<"The Value of var1 is "<<var1<<"\n";
+  cout<<var2<<"\n";
+  cout<<"The Value of var2 is "<<var2<<"\n";
+  cout<<no<<"\n";
+  cout<<"The Value of no is "<<no<<"\n";
+  cout<<point<<"\n";
+  cout<<"The Value of point is "<<point<<"\n";
+  cout<<ch<<"\n";
+  cout<<"The Value of ch is "<<ch<<"\n";
+  cout<<str<<"\n";
+  cout<<"The Value of str is "<<str<<"\n";
+  cout<<longPoint<<"\n";
+  cout<<"The Value of longPoint is "<<longPoint<<"\n";
+  cout<<is<<"\n";
+  cout<<"The Value of is"<<is;
+
+  return 0;
+}
