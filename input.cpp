7159256614a6a7c9cd97ec854e7859fff581ca34
areeba-amirr <areebaamir826@gmail.com>
2025-09-13 23:54:16 +0500

    Input from user

---
+++ input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a , b;
+  float no1,no2;
+  cout<<"Enter Number One:";
+  cin>>no1;
+  cout<<"Enter Number Two:";
+    cin>>no2;
+  cout<<"No One:"<<no1<<" No Two:"<<no2;
+  cin>>a;
+  cin>>b;
+  cout<<a<<endl<<b;
+	return 0;
+}
