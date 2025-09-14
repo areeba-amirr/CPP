43e4919cad7e8a0dc075fdf47a718d6a6756046b
areeba-amirr <areebaamir826@gmail.com>
2025-09-14 14:38:09 +0500

    CIN

---
+++ userInput.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string name;
+	int age;
+	cout<<"Enter You Name:";
+	getline(cin,name);
+	cout<<"Enter Your Age:";
+	cin>>age;
+	cout<<"Hello,"<<name<<"! You are "<<age<<" years old";
+	return 0;
+}
+
