5623ff288e9ad5ab52c5be77c04e8cc4b04d9025
areeba-amirr <areebaamir826@gmail.com>
2025-09-14 14:37:35 +0500

    COUT

---
+++ print.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"I am Areeba Aamir"<<endl;
+	cout<<"Fun Fact: My superpower is surviving endless study hours as long as coffee is my sidekick"<<endl;
+	cout<<"I am learning C++ to understand OOP";
+	return 0;
+}
+
+
