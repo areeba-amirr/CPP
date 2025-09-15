4e3771605325d00f700e07e55ea1708f40083c21
areeba-amirr <areebaamir826@gmail.com>
2025-09-15 23:07:55 +0500

    For,while and do while

---
+++ loops.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//For Loop
+	for(int i = 0; i<= 10;i++){
+		cout<<i<<endl;
+	}
+	//while loop
+	int i = 0;
+	while(i <= 10){
+		cout<<i<<endl;
+		i++;
+	}
+//    Do-While Loop
+    int j = 0;
+    do{
+    	cout<<j<<endl;
+    	j++;
+	}
+	while(j <= 10);
+return 0; 
+}
