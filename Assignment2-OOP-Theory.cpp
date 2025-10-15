c904a896d7df92c645442eec394a34a097ff1d40
areeba-amirr <areebaamir826@gmail.com>
2025-10-15 23:23:19 +0500

    Encapsulation+Polymorphism+Inheritance

---
+++ Assignment2-OOP-Theory.cpp
@@ -0,0 +1,3 @@
+/*Q1. School Attendance System A school wants to record daily attendance of students. Create a class Attendance that stores studentName, rollNo, and status (Present/Absent).
+ Use a constructor to initialize data and a member function to display attendance details. Demonstrate creating multiple student objects and showing attendance.*/
+ 
